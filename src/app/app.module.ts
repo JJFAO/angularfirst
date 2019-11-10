30cc7d6ce77e6a58135034e0148f4f294c90df0b
JJFAO <jjfalonso@gmail.com>
2019-11-10 05:00:38 -0300

    proyect start up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SliderComponent } from './slider/slider.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SliderComponent,
+    PhotosComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule
